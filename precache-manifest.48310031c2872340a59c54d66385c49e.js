9aa1ec99397487af66c2840328f8cb3c62a9b3d4
GitHubForDmitry <dmitrykoka@gmail.com>
2019-08-05 16:02:43 +0300

    Updates

---
+++ precache-manifest.48310031c2872340a59c54d66385c49e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64467b5967583d344bf2bada2e041f37",
+    "url": "/react_tabs/index.html"
+  },
+  {
+    "revision": "7c2ad3af6084d1b090a4",
+    "url": "/react_tabs/static/css/main.088b3361.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react_tabs/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "7c2ad3af6084d1b090a4",
+    "url": "/react_tabs/static/js/main.00dfd02a.chunk.js"
+  },
+  {
+    "revision": "010ecae327fc4474afd9",
+    "url": "/react_tabs/static/js/runtime~main.310f5775.js"
+  }
+]);
